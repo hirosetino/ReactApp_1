ab80de0dd309e0a2f260b1817979b070b296d564
hirosetino <hirosekeita0524@gmail.com>
2025-11-26 12:54:04 +0900

    修正

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,8 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        // ローカル・開発用には '*' を使うより env 判定推奨
+        if (env('APP_ENV') !== 'local') {
+            $middleware->trustProxies(
+                proxies: '*',
+                headers: Request::HEADER_X_FORWARDED_ALL
+            );
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
