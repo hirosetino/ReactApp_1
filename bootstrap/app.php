a5ff2abe3814e49f240e3b6ee24d190195e7943a
hirosetino <hirosekeita0524@gmail.com>
2025-11-26 13:19:09 +0900

    修正

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpFoundation\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,10 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        // 本番のみ
         if (env('APP_ENV') !== 'local') {
-            // Laravel 12 では HEADER_X_FORWARDED_ALL は存在しないため文字列で
-            $middleware->trustProxies('*', 'X_FORWARDED_FOR|X_FORWARDED_HOST|X_FORWARDED_PROTO|X_FORWARDED_PORT');
+            // Symfony の定数を使う
+            $middleware->trustProxies('*', Request::HEADER_X_FORWARDED_ALL);
         }
     })
     ->withExceptions(function ($exceptions) {
