ffbdc6723a822ae89980758c6a1cbcaa7fbf66b0
hirosetino <hirosekeita0524@gmail.com>
2025-12-14 21:19:11 +0900

    修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'cooking pad') }}</title>
+        <link rel="icon" type="image/png" href="/images/logo_icon.png">
+        <link rel="apple-touch-icon" href="/images/logo_icon.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
